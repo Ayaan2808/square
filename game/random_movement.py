55453e5e7e6dbbc1b79a68d00f5346ae4b8ca715
Ayaan2808 <ayaanvanitatushar@gmail.com>
2022-04-22 20:43:11 +0400

    burger game

---
+++ game/random_movement.py
@@ -0,0 +1,55 @@
+import pygame
+import random
+
+pygame.init()
+
+height = 600
+width = 600
+red = (255,0,0)
+x = 520
+y = 520
+xs = 30
+ys = 30
+score = 0
+
+screen = pygame.display.set_mode((height, width))
+runner = True
+
+def game():
+    global xs
+    global runner
+    global ys
+    global score
+    man = pygame.image.load("img/user.png")
+    rect = man.get_rect()
+    rect.center = (x,y)
+    man  = pygame.transform.scale(man, (60,60))
+    screen.blit(man, rect)
+    burger = pygame.image.load("img/burger.png")
+    rects = burger.get_rect()
+    rects.center = (xs,ys)
+    burger = pygame.transform.scale(burger,(60,60))
+    screen.blit(burger, rects)
+    if pygame.Rect.colliderect(rect,rects):
+        xs = random.randrange(50,550)
+        ys = random.randrange(50,550)
+        score = score + 1
+    font = pygame.font.Font('freesansbold.ttf', 40)
+    text = font.render(f"score : {score}", True, (0,0,255))
+    screen.blit(text, (280, 10))
+while runner:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            runner = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                y = y - 20
+            elif event.key == pygame.K_LEFT:
+                x = x - 20
+            elif event.key == pygame.K_DOWN:
+                y = y + 20
+            elif event.key == pygame.K_RIGHT:
+                x = x + 20
+    screen.fill(red)
+    game()
+    pygame.display.update()
--- game/shooter_game.py
+++ game/shooter_game.py
@@ -2,26 +2,60 @@ import pygame
 
 pygame.init()
 
-height = 300
+height = 600
 width = 600
 white = (255,255,255)
 blue = (0,0,255)
+x = 250
+y = 450
 
 screen = pygame.display.set_mode((height,width))
 
 runner = True
 
-def bullets():
-    img = pygame.image.load("img/bullet.png")
-    img = pygame.transform.scale(img, (50,50))
-    pygame.sprite.draw(screen, bgd=None)
+def spaceship(x,y):
+    ship = pygame.image.load("img/ship.png")
+    ship = pygame.transform.scale(ship, (100, 100))
+    screen.blit(ship, (x,y))
+
+class explosion(pygame.sprite.Sprite):
+    def __init__ (self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        for num in range(1,6):
+            img = pygame.image.load(f"img/exp{num}.png")
+            img = pygame.transform.scale(img, (100,100))
+            self.images.append(img)
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x,y]
+        self.count = 0
+    def update(self):
+        explosion_speed = 30
+        self.count += 1
+        if self.count >= explosion_speed and self.index < len(self.images) - 1:
+            self.count = 0
+            self.index += 1
+            self.image = self.images[self.index]
+        if self.index >= len(self.images) - 1 and self.count >= explosion_speed:
+            self.kill()
+explosion_group = pygame.sprite.Group()
 
 while runner:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runner = False
-        if event.type == pygame.K_SPACE:
-            bullets() 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                Explosion = explosion(x+40, y-120)
+                explosion_group.add(Explosion)
+            if event.key == pygame.K_LEFT:
+                x = x - 30
+            if event.key == pygame.K_RIGHT:
+                x = x + 30
     screen.fill(blue)
-    square = pygame.draw.rect(screen, white, pygame.Rect(100, 100, 100, 100))
+    spaceship(x,y)
+    explosion_group.draw(screen)
+    explosion_group.update()
     pygame.display.update()
